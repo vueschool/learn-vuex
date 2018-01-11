81e17fdee0f66ea8caab91ee73b4cc048f09196f
Alex Kyriakidis <hootlex@icloud.com>
2018-01-11 20:10:36 +0200

    mutations

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -9,16 +9,18 @@
 
 <script>
   import shop from '@/api/shop'
+  import store from '@/store/index'
+
   export default {
-    data () {
-      return {
-        products: []
+    computed: {
+      products () {
+        return store.state.products
       }
     },
 
     created () {
       shop.getProducts(products => {
-        this.products = products
+        store.commit('setProducts', products)
       })
     }
   }
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 
 Vue.use(Vuex)
 
-new Vuex.Store({
+export default new Vuex.Store({
   state: { // = data
     products: []
   },
@@ -17,12 +17,14 @@ new Vuex.Store({
   actions: {
     fetchProducts () {
       // make the call
+      // call setProducts mutation
     }
   },
 
   mutations: {
-    setProducts () {
+    setProducts (state, products) {
       // update products
+      state.products = products
     }
   }
 })
