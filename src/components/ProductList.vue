5ac9ec9ed106d872e475f589236ffaef743d6869
Alex Kyriakidis <hootlex@icloud.com>
2018-01-24 15:14:02 +0200

    show product inventory

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -7,7 +7,7 @@
       >
       <ul v-else>
         <li v-for="product in products">
-          {{product.title}} - {{product.price}}
+          {{product.title}} - {{product.price}} - {{product.inventory}}
           <button @click="addProductToCart(product)">Add to cart</button>
         </li>
       </ul>
