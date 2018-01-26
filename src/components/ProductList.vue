a6e6a332c779ad9e6acfa4813a15e1c0de247a24
Alex Kyriakidis <hootlex@icloud.com>
2018-01-26 18:49:32 +0200

    dynamic getters

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -8,7 +8,10 @@
       <ul v-else>
         <li v-for="product in products">
           {{product.title}} - {{product.price | currency}} - {{product.inventory}}
-          <button @click="addProductToCart(product)">Add to cart</button>
+          <button
+            :disabled="!productIsInStock(product)"
+            @click="addProductToCart(product)"
+          >Add to cart</button>
         </li>
       </ul>
     </div>
@@ -24,7 +27,11 @@
 
     computed: {
       products () {
-        return this.$store.getters.availableProducts
+        return this.$store.state.products
+      },
+
+      productIsInStock () {
+        return this.$store.getters.productIsInStock
       }
     },
 
--- src/store/index.js
+++ src/store/index.js
@@ -29,6 +29,12 @@ export default new Vuex.Store({
 
     cartTotal (state, getters) {
       return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
+    },
+
+    productIsInStock () {
+      return (product) => {
+        return product.inventory > 0
+      }
     }
   }
   ,
@@ -45,15 +51,15 @@ export default new Vuex.Store({
       })
     },
 
-    addProductToCart (context, product) {
-      if (product.inventory > 0) {
-        const cartItem = context.state.cart.find(item => item.id === product.id)
+    addProductToCart ({state, getters, commit}, product) {
+      if (getters.productIsInStock(product)) {
+        const cartItem = state.cart.find(item => item.id === product.id)
         if (!cartItem) {
-          context.commit('pushProductToCart', product.id)
+          commit('pushProductToCart', product.id)
         } else {
-          context.commit('incrementItemQuantity', cartItem)
+          commit('incrementItemQuantity', cartItem)
         }
-        context.commit('decrementProductInventory', product)
+        commit('decrementProductInventory', product)
       }
     },
 
